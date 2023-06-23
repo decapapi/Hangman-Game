c64816e357d7727d60e3c0ecfab23d446d7f9da8
erdecapapi <erdecapapi@gmail.com>
2023-06-23 22:14:39 +0200

    Various fixes and improvements
    - Fixed checkInput function being duplicated.
    - Fixed first word not taking in to account difficulty set in settings.
    - Draw "Wrong Letters" and "Game Over"/"You won" text inside the svg

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,4 @@
 const hiddenWord = document.getElementById('hidden-word');
-const wrongLetters = document.getElementById('wrong-letters');
 
 const input = document.getElementById('input');
 const languageSelector = document.getElementById('langauge');
@@ -19,11 +18,14 @@ document.getElementById('hint').addEventListener('click', hint);
 document.getElementById('reset').addEventListener('click', restart);
 
 var usedLetters = [];
+var wrongLetters = [];
 var timesFailed = 0;
 var hits = 0;
 var hints = 1;
 
-var choosenWord = getRandomWord();
+var choosenWord;
+
+var wrongLettersText;
 
 window.onload = startGame;
 
@@ -53,8 +55,13 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function drawGallow() {
+function updateWrongLettersText() {
+    wrongLettersText.attr({
+      text: "Wrong letters: " + wrongLetters.join(", ")
+    });
+  }
 
+function drawGallow() {
     const mastile = s.rect(gallow.offsetWidth / 3.5, gallow.offsetHeight * 0.3, gallow.offsetWidth / 50, gallow.offsetHeight * 0.6);
     const base = s.rect(gallow.offsetWidth / 3.95, gallow.offsetHeight * 0.87, gallow.offsetHeight * 0.2, gallow.offsetWidth / 60);
     const top = s.rect(gallow.offsetWidth / 3.5, gallow.offsetHeight * 0.29, gallow.offsetHeight * 0.3, gallow.offsetWidth / 60);
@@ -65,6 +72,13 @@ function drawGallow() {
       stroke: 'black'
     });
   
+    wrongLettersText = s.text(gallow.offsetWidth / 1.5, gallow.offsetHeight * 0.9, "Wrong letters:");
+    wrongLettersText.attr({
+      "font-size": gallow.offsetWidth / 45,
+      "font-family": "Quicksand, sans-serif",
+      "text-anchor": "start"
+    });
+
     // Append the elements to the SVG
     s.append(mastile, base, top, support);
 }
@@ -74,26 +88,33 @@ function startGame() {
 
     if (localStorage.getItem('difficulty') !== null)
         difficultySelector.value = localStorage.getItem('difficulty');
+
+    if (localStorage.getItem('language') !== null)
+        languageSelector.value = localStorage.getItem('language');
+
+    choosenWord = getRandomWord();
 }
 
 function getRandomWord() {
     let word;
     let wordLength;
-    let wordLanguage;
 
     switch (difficultySelector.value) {
         case 'easy':
-        wordLength = getRandomInt(3, 5);
-        break;
+            wordLength = getRandomInt(3, 5);
+            break;
         case 'medium':
-        wordLength = getRandomInt(6, 8);
-        break;
+            wordLength = getRandomInt(6, 8);
+            break;
         case 'hard':
-        wordLength = getRandomInt(9, 12);
-        break;
+            wordLength = getRandomInt(9, 12);
+            break;
         case 'expert':
-        wordLength = getRandomInt(13, 15);
-        break;
+            wordLength = getRandomInt(13, 15);
+            break;
+        default:
+            wordLength = getRandomInt(3, 15);
+            break;
     }
 
     fetch(`https://random-word-api.herokuapp.com/word?length=${Math.floor(wordLength)}&&lang=${languageSelector.value}`)
@@ -110,15 +131,16 @@ function getRandomWord() {
 function restart() {
     if (confirm("This action will reset the game. Are you sure?")) {
         usedLetters.splice(0, usedLetters.length);
+        wrongLetters.splice(0, wrongLetters.length);
         timesFailed = 0;
         hits = 0;
         hints = 1;
         choosenWord = getRandomWord();
         hiddenWord.innerHTML = '';
-        wrongLetters.innerText = 'Wrong letters:';
         svg.innerHTML = '';
         drawGallow();
         difficultySelector.dataset.previousValue = difficultySelector.value;
+        updateWrongLettersText();
     } else {
         difficultySelector.value = difficultySelector.dataset.previousValue == null ? "medium" : difficultySelector.dataset.previousValue;
     }
@@ -126,7 +148,25 @@ function restart() {
 
 function update() {
     if (timesFailed >= 7) {
-        hiddenWord.innerHTML = choosenWord + '<br> <span style="color: red; letter-spacing: normal;">Game over!</span>';
+        var text = s.text(gallow.offsetWidth / 2, gallow.offsetHeight * 0.17, "Game Over!");
+        text.attr({
+            "font-size": gallow.offsetWidth / 25,
+            "font-family": "Quicksand, sans-serif",
+            "font-weight" : "bold",
+            "fill": "red",
+            "text-anchor": "middle"
+        });
+    }
+    
+    if (hits === choosenWord.length) {
+        var text = s.text(gallow.offsetWidth / 2, gallow.offsetHeight * 0.17, "You Won!");
+        text.attr({
+            "font-size": gallow.offsetWidth / 25,
+            "font-family": "Quicksand, sans-serif",
+            "font-weight" : "bold",
+            "fill": "green",
+            "text-anchor": "middle"
+        });
     }
 
     if (timesFailed >= 1) {
@@ -190,52 +230,46 @@ function update() {
 }
 
 function checkInput() {
-    const letter = input.value.toLowerCase();
+    if (choosenWord == null || choosenWord == undefined ||
+        timesFailed >= 7 || hits == choosenWord.length)
+        return;
 
-    if (letter.length === 0 || hasNumbers(letter)) {
-        alert("Please enter a valid letter.");
+    var userInput = input.value.toLowerCase();;
+
+    if (userInput.length != 1) {
+        alert('You must enter one letter each time!');
+        return;
+    }
+  
+    if (hasNumbers(userInput)) {
+        alert("You can't use numbers!");
         return;
     }
 
-    if (usedLetters.includes(letter)) {
-        alert("You already guessed that letter.");
+    if (usedLetters.includes(userInput.toLowerCase())) {
+        alert('That letter has already been used!');
         return;
+    } else {
+        usedLetters.push(userInput.toLowerCase());
     }
-
-    usedLetters.push(letter);
-
-    if (choosenWord.includes(letter)) {
-        let temp = hiddenWord.innerText;
-
-        for (let i = 0; i < choosenWord.length; i++) {
-        if (choosenWord[i] === letter) {
-            temp = replaceChar(temp, i, letter);
+  
+    let correct = false;
+    for (let i = 0; i <= choosenWord.length; i++) {
+        if (choosenWord.toLowerCase()[i] == userInput.toLowerCase()) {
+            hiddenWord.innerText = replaceChar(hiddenWord.innerText, i, userInput.toLowerCase());
+            correct = true;
             hits++;
         }
-        }
-
-        hiddenWord.innerText = temp;
-
-        if (hits === choosenWord.length) {
-        hiddenWord.innerHTML += '<br> <span style="color: green; letter-spacing: normal;">You won!</span>';
-        input.disabled = true;
-        checkButton.disabled = true;
-        }
-    } else {
-        timesFailed++;
-        wrongLetters.innerText += letter;
-
-        if (timesFailed === 7) {
-        update();
-        hiddenWord.innerHTML = choosenWord + '<br> <span style="color: red; letter-spacing: normal;">Game over!</span>';
-        input.disabled = true;
-        checkButton.disabled = true;
-        } else {
-        update();
-        }
     }
-
+  
+    if (!correct) {
+        timesFailed++;
+        wrongLetters.push(userInput.toLowerCase());
+        updateWrongLettersText();
+    }
+  
     input.value = '';
+    update();
 }
 
 function hint() {
@@ -263,57 +297,6 @@ function hint() {
 
     hints--;
 }
-
-function checkInput() {
-    if (choosenWord == null || choosenWord == undefined ||
-      timesFailed >= 7 || hits == choosenWord.length)
-      return;
-
-    var userInput = input.value;
-
-    if (userInput.length != 1) {
-      alert('You must enter one letter each time!');
-      return;
-    }
-  
-    if (hasNumbers(userInput)) {
-      alert("You can't use numbers!");
-      return;
-    }
-
-    if (usedLetters.includes(userInput.toLowerCase())) {
-      alert('That letter has already been used!');
-      return;
-    } else {
-      usedLetters.push(userInput.toLowerCase());
-    }
-  
-    let correct = false;
-    for (let i = 0; i <= choosenWord.length; i++) {
-      if (choosenWord.toLowerCase()[i] == userInput.toLowerCase()) {
-        hiddenWord.innerText = replaceChar(hiddenWord.innerText, i, userInput.toLowerCase());
-        correct = true;
-        hits++;
-      }
-    }
-  
-    if (!correct) {
-      if (timesFailed === 0)
-        wrongLetters.innerText += ' ' + userInput;
-      else
-        wrongLetters.innerText += ', ' + userInput;
-        
-      timesFailed++;
-      update();
-    }
-  
-    if (hits == choosenWord.length) {
-      hiddenWord.innerHTML = choosenWord + '<br> <span style="color: green;">You won!</span>';
-    }
-  
-    input.value = '';
-}
-
 
 languageSelector.addEventListener('change', function() {
     localStorage.setItem('language', languageSelector.value);
